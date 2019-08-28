1e89685c0fa7fab99e4e778382107dd10d5d2d5c
jianglang <643546122@qq.com>
2019-08-28 18:14:26 +0800

    添加首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -13,34 +13,58 @@ let PageData = {
 
 Page({
   data: Object.assign({
-    userInfo: {},
-    hasUserInfo: false,
-    loadBox: false,
-    bgmBtn: false,
-    bgmPlay: false
+    imgUrls: [
+      '/images/index/banner1.png',
+      '/images/index/banner2.png',
+      '/images/index/banner3.png'
+    ],
+    swiperImgUrls: [1, 2, 3],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    swiperCurrent: 0
   }, PageData), //页面的初始数据
   async onLoad(option) {
     $page = this;
     $query = option;
-    console.log('getQueryString', option);
-    await beats.member.getUserInfo();
   },
-  onReady: function () { }, //监听页面初次渲染完成
-  onShow: function () {
+  onReady: function() {}, //监听页面初次渲染完成
+  onShow: function() {
     // if (app.bgm){
     //     app.bgm.reShow();
     // }//edn if
   }, //监听页面显示
-  onHide: function () { }, //监听页面隐藏
-  onUnload: function () { }, //监听页面卸载
-  onPullDownRefresh: function () { }, //页面相关事件处理函数--监听用户下拉动作
-  onReachBottom: function () { }, //页面上拉触底事件的处理函数
-  onShareAppMessage: function () { //用户点击右上角分享
+  onHide: function() {}, //监听页面隐藏
+  onUnload: function() {}, //监听页面卸载
+  onPullDownRefresh: function() {}, //页面相关事件处理函数--监听用户下拉动作
+  onReachBottom: function() {}, //页面上拉触底事件的处理函数
+  onShareAppMessage: function() { //用户点击右上角分享
     return app.setShareData();
   },
-  bgmClick: function () {//背景音乐按钮点击事件
+  bgmClick: function() { //背景音乐按钮点击事件
     app.bgm.click();
   },
+  /**
+   * 轮播自动滑动时，获取当前的轮播id
+   */
+  swiperChange(e) {
+    const that = this;
+    that.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+
+  /**
+   * 图片手动滑动时，获取当前的轮播id
+   */
+  imageChange(e) {
+    const that = this;
+    console.log(e.currentTarget.id)
+    that.setData({
+      swiperCurrent: e.currentTarget.id
+    })
+  }
 }) //end page
 
 //-------------------------------------------------------业务逻辑-------------------------------------------------------
